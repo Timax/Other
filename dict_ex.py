44f43e608468ce1c366c7ae1cb1659c20b1ed369
Kolya <kulemzin.nick@gmail.com>
2021-12-08 20:19:17 +0300

    Обновили файл словаря

--- dict_ex.py
+++ dict_ex.py
@@ -24,3 +24,24 @@ D4 = {'name' : {'first' : 'Kolya', 'last' : 'Kulemzin'}, 'role' :['analyst', 'de
 
 print(D4)
 print(D4['name']['last'], D4['role'][1])
+
+#Проверка на пустоту
+
+D5 = {'a' : 1,  'b' : 3, 'c' :17}
+print(D5)
+
+if not 'f' in D5:
+	print('missing')
+else:
+	print(D5['f'])
+	
+value1 = D5.get('a', 0)
+value2 = D5.get('e', 0)
+print(value1, value2)
+
+value3 = D5['x'] if 'x' in D5 else 0
+print(value3)
+
+#Сортировка ключей
+
+
